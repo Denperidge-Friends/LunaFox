f5726c10434839329d867a1d2ceb6c82b99f1fab
Denperidge <denperidge@gmail.com>
2023-06-14 03:06:58 +0200

    Implemented playlist

--- src/music.js
+++ src/music.js
@@ -1,25 +1,35 @@
-import { Howl, Howler } from "howler";
+import { Howl } from "howler";
 
 const play = document.getElementById("play");
 const volume = document.getElementById("volume");
 
-let sound = new Howl({
-    src: songs,
-    autoplay: true,
-    volume: volume.value / 100,
-    loop: true,
-    onplay: () => {
-        play.innerText = "pause"
-    },
-    onpause: () => {
-        play.innerText = "play"
+let songIndex = -1;
+
+// Playlist functionality based on https://github.com/goldfire/howler.js/issues/191
+function nextSong() {
+    songIndex++;
+    if (songIndex >= songs.length) {
+        songIndex = 0;
     }
-});
+
+    return new Howl({
+        src: songs[songIndex],
+        autoplay: true,
+        volume: volume.value / 100,
+        onplay: () => {
+            play.innerText = "pause"
+        },
+        onpause: () => {
+            play.innerText = "play"
+        },
+        onend: nextSong
+    });
+}
+let sound = nextSong();
 
 volume.addEventListener("change", (event) => {
     sound.volume(event.target.value/100);
 });
-
 
 document.getElementById("play").addEventListener("click", () => {
     if(sound.playing()) {
@@ -29,3 +39,7 @@ document.getElementById("play").addEventListener("click", () => {
     }
 });
 
+document.getElementById("next").addEventListener("click", () => { 
+    sound.unload();
+    sound = nextSong();
+});
