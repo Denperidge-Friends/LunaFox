50ff6ae02afa8897dffcd7bfd446271128b9eb8b
Denperidge <denperidge@gmail.com>
2023-06-14 02:32:05 +0200

    Fade-in posts

--- src/index.js
+++ src/index.js
@@ -2,13 +2,21 @@ let buttons = document.querySelectorAll("nav button");
 
 const originalTitle = document.title;
 
+function delayedValue(elem, attribute, value, delay=100) {
+    setTimeout(() => {
+        elem.style[attribute] = value;
+    }, delay);
+}
+
 function toggleDisplay(id) {
     let elem = document.getElementById(id);
     if (elem.style.display == "block") {
-        elem.style.display = "none";
+        elem.style.opacity = 0;
+        delayedValue(elem, "display", "none", 2000);
         document.title = originalTitle;
     } else {
         elem.style.display = "block";
+        delayedValue(elem, "opacity", 1);
         document.title = "LUNAFOX('s secret blog!)!!"
     }
 }
