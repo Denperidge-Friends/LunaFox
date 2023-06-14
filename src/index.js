b040f25a5700790eb22ba3a36ad03d93989442ca
Denperidge <denperidge@gmail.com>
2023-06-14 02:18:45 +0200

    Basic title, styled hr + button + .range + #music

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 let buttons = document.querySelectorAll("nav button");
 
+const originalTitle = document.title;
+
 function toggleDisplay(id) {
     let elem = document.getElementById(id);
     if (elem.style.display == "block") {
         elem.style.display = "none";
+        document.title = originalTitle;
     } else {
         elem.style.display = "block";
+        document.title = "LUNAFOX('s secret blog!)!!"
     }
 }
 
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", (e) => {
-        console.log(document.getElementById(e.target.value))
         toggleDisplay(e.target.value);
     });
 }
