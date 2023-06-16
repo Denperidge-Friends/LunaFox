052feef7b72e9f2f1e430df50f21b5c9b53a82d3
Denperidge <denperidge@gmail.com>
2023-06-16 14:26:01 +0200

    Music.js now uses jQuery, added prev

--- src/music.js
+++ src/music.js
@@ -1,43 +1,59 @@
 import { Howl } from "howler";
+import $ from "jquery";
 
-const play = document.getElementById("play");
-const volume = document.getElementById("volume");
+const play = $("#play");
+const volume = $("#volume");
+
+let sound;
 
 function random(min, max) {
     // See https://www.w3schools.com/JS/js_random.asp
     return Math.floor(Math.random() * (max - min) ) + min;
 }
 
-let songIndex = random(-1, songs.length - 1);  // -1, as nextSong wil do ++
+let songIndex = random(0, songs.length);
 
 
 // Playlist functionality based on https://github.com/goldfire/howler.js/issues/191
-function nextSong() {
-    songIndex++;
+function playSong() {
     if (songIndex >= songs.length) {
         songIndex = 0;
+    } else if (songIndex < 0) {
+        songIndex = songIndex.length - 1;
     }
 
     return new Howl({
         src: songs[songIndex],
         autoplay: true,
-        volume: volume.value / 100,
+        volume: volume.val() / 100,
         onplay: () => {
-            play.innerText = "pause"
+            play.text("pause");
         },
         onpause: () => {
-            play.innerText = "play"
+            play.text("play");
         },
         onend: nextSong
     });
 }
-let sound = nextSong();
 
-volume.addEventListener("change", (event) => {
+function nextSong() {
+    songIndex++;
+    sound.unload();
+    sound = playSong();
+}
+
+function prevSong() {
+    songIndex--;
+    sound.unload();
+    sound = playSong();
+}
+
+
+volume.on("change", (event) => {
     sound.volume(event.target.value/100);
 });
 
-document.getElementById("play").addEventListener("click", () => {
+play.on("click", () => {
     if(sound.playing()) {
         sound.pause();
     } else {
@@ -45,7 +61,7 @@ document.getElementById("play").addEventListener("click", () => {
     }
 });
 
-document.getElementById("next").addEventListener("click", () => { 
-    sound.unload();
-    sound = nextSong();
-});
+$("#prev").on("click", prevSong);
+$("#next").on("click", nextSong);
+
+sound = playSong();
